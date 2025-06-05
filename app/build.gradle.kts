d7741a76091aa9180ec94016af4bcef7729f188b
vnmeue <mnv07@proton.me>
2025-06-05 14:04:21 +0530

    RC1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 31
-        versionName = "0.0.69"
+        versionName = "0.0.69-RC1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
