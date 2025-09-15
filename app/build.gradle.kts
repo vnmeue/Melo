17cc96d92ad8e8e64929e92965992c1ee2cde9c7
qxrein <manavrj.07@gmail.com>
2025-09-15 14:40:06 +0530

    v0.80.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 31
-        versionName = "0.0.78"
+        versionName = "0.0.80"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
